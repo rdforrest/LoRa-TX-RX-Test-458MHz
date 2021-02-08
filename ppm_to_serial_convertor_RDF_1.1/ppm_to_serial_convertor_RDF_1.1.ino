eac5ea2d4690380f23b2e0b71e6a904e80c283c8
David Forrest <33370970+rdforrest@users.noreply.github.com>
2021-02-08 12:03:44 +0000

    Update ppm_to_serial_convertor_RDF_1.1.ino

--- ppm_to_serial_convertor_RDF_1.1/ppm_to_serial_convertor_RDF_1.1.ino
+++ ppm_to_serial_convertor_RDF_1.1/ppm_to_serial_convertor_RDF_1.1.ino
@@ -1,5 +1,6 @@
 /*
    Four channels but could easliy be made to cope with more.
+   Don't use this in conjunction with radio module - interrupt clashes! Use the Accurateppm routine instead.
    For Nano Every (Which has the advantage of having several serial ports in hardware)(I have also had problems with clones stopping working and requiring sketch to be reloaded.)
    Arduino serial data default is, serial baud rate (9600) and 8 data bits, no parity,no stop,LSB and not inverted - the default
 */
