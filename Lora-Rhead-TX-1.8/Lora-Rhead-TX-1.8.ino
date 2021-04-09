ad963715b45640263fe449b95745653fd0ccf78b
David Forrest <33370970+rdforrest@users.noreply.github.com>
2021-04-09 13:28:44 +0100

    Add files via upload

---
+++ Lora-Rhead-TX-1.8/Lora-Rhead-TX-1.8.ino
@@ -0,0 +1,20 @@
+/*
+   https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/rfm9x-test
+  // LoRa 9x_TX
+  // -*- mode: C++ -*-
+  // Example sketch showing how to create a simple messaging client (transmitter)
+  // with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+  // reliability, so you should only use RH_RF95 if you do not need the higher
+  // level messaging abilities.
+  // It is designed to work with the other example LoRa9x_RX
+
+  /*
+  RadioHead library );
+  http://www.airspayce.com/mikem/arduino/RadioHead/
+
+  Use of ACK:
+  https://lowpowerlab.com/forum/moteino/how-to-enforce-and-read-ack/
+  https://lowpowerlab.com/forum/moteino/sending-data-back-with-ack/
+  https://lowpowerlab.com/forum/moteino/getting-started-with-sending-radiowireless-payloads/
+  https://lowpowerlab.com/forum/rf-range-antennas-rfm69-library/how-to-transmit-integer-without-converting-to-string/msg28724/#msg28724
+*/
